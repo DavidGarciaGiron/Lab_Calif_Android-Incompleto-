9fa2ce79e3982a6dbac2ab637fa9542079a2cade
DavidGarciaGiron <david.garcia@tecsup.edu.pe>
2018-11-12 18:55:19 -0500

    laboratorio calificado de android terminado

--- MatriculaApp/app/src/main/java/com/example/david/matriculaapp/ApiService.java
+++ MatriculaApp/app/src/main/java/com/example/david/matriculaapp/ApiService.java
@@ -2,8 +2,15 @@ package com.example.david.matriculaapp;
 
 import java.util.List;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface ApiService {
 
@@ -12,4 +19,18 @@ public interface ApiService {
     @GET("/api/personas/")
     Call<List<Persona>> getProductos();
 
+    @FormUrlEncoded
+    @POST("/api/personas/")
+    Call< Persona> createPersona(@Field("tipo") String tipo,
+                                   @Field("correo") String correo,
+                                   @Field("detalle") String detalle);
+    @Multipart
+    @POST("/api/personas/")
+    Call< Persona> createPersonaWithImage(
+            @Part("tipo") RequestBody tipo,
+            @Part("correo") RequestBody correo,
+            @Part("detalle") RequestBody detalle,
+            @Part MultipartBody.Part imagen
+            
+    );
 }
